78cddedac2770c2b5794294ee1bce5a7f629d129
alxFv <worfysito15@gmail.com>
2025-01-10 17:04:57 -0500

    feat: cambios detectados nueva funcionalidad en page.tsx

--- src/app/(admin)/(catalog)/product/page.tsx
+++ src/app/(admin)/(catalog)/product/page.tsx
@@ -1,3 +1,14 @@
-export default function page() {
-	return <div>page</div>;
+import { getAllProducts } from "./actions";
+
+export default async function Page() {
+
+	const products = await getAllProducts();
+
+	console.log(products)
+
+    return (
+        <div>
+			{JSON.stringify(products)}
+        </div>
+    );
 }
