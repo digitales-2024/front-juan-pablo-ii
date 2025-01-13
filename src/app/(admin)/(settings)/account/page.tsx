42c747201cba3c3ab12bb0a1e44e42b8478a1a87
JMNRA <jmedinan@unsa.edu.pe>
2025-01-13 14:49:24 -0500

    feat: agregar componente FormProfile y su integración en la página de cuenta

---
+++ src/app/(admin)/(settings)/account/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FormProfile from './_components/FormProfile'
+
+export default function AccountPage() {
+  return (
+    <div>
+      <FormProfile></FormProfile>
+    </div>
+  )
+}
