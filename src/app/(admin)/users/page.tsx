a735fd608778cb56612678acc05eac5c6e4036a2
EleoRomario <eleo.romario@gmail.com>
2025-01-14 11:21:21 -0500

    feat: agregar funciones para crear, actualizar y eliminar usuarios, y generar contraseñas

--- src/app/(admin)/users/page.tsx
+++ src/app/(admin)/users/page.tsx
@@ -1,4 +1,10 @@
+import { serverFetch } from "@/utils/serverFetch";
+import { User } from "./types";
+import { UsersTable } from "./_components/UsersTable";
+
 export default async function PageUser() {
+	const [user, error] = await serverFetch<User[]>("/users");
+
 	return (
 		<>
 			<div className="mb-2 flex items-center justify-between space-y-2 flex-wrap gap-x-4">
@@ -11,7 +17,9 @@ export default async function PageUser() {
 					</p>
 				</div>
 			</div>
-			<div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0"></div>
+			<div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
+				<UsersTable data={user} />
+			</div>
 		</>
 	);
 }
--- src/app/(admin)/users/types.ts
+++ src/app/(admin)/users/types.ts
@@ -1,5 +1,18 @@
 import { components } from "@/types/api";
+import { z } from "zod";
 export type RolResponseDto = components["schemas"]["RolResponseDto"];
 export type User = Omit<components["schemas"]["UserResponseDto"], "roles"> & {
 	roles: RolResponseDto[];
 };
+export type UserCreateDto = components["schemas"]["CreateUserDto"];
+
+export const userCreateSchema = z.object({
+	name: z.string().min(1, { message: "El nombre es requerido" }),
+	email: z.string().min(1, { message: "El email es requerido" }),
+	password: z.string().min(1, { message: "La contraseña es requerida" }),
+	phone: z
+		.string()
+		.min(1, { message: "El telefono es requerido" })
+		.optional(),
+	roles: z.array(z.string()).min(1, { message: "El rol es requerido" }),
+}) satisfies z.ZodType<UserCreateDto>;
