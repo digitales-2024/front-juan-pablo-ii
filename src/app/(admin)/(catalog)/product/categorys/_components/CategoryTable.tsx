a4ae1901febaa1cf45fbd9003efa0baaf7bd11cb
alxFv <worfysito15@gmail.com>
2025-01-16 11:37:13 -0500

    feat: cambios detectados nueva funcionalidad en CategoryTable.tsx

--- src/app/(admin)/(catalog)/product/categorys/_components/CategoryTable.tsx
+++ src/app/(admin)/(catalog)/product/categorys/_components/CategoryTable.tsx
@@ -12,22 +12,40 @@ interface CategoryTableProps {
     profile: Profile;
 }
 
+/**
+ * Componente que renderiza una tabla de categorías.
+ * Recibe una lista de categorías y el perfil del usuario actual.
+ * Utiliza la hook `useProfileStore` para guardar el perfil del usuario actual.
+ * Utiliza la hook `useMemo` para memoizar las columnas de la tabla.
+ * Utiliza la hook `useEffect` para guardar el perfil del usuario actual.
+ * @param {Category[]} data - La lista de categorías.
+ * @param {Profile} profile - El perfil del usuario actual.
+ * @returns {JSX.Element} El componente renderizado.
+ */
 const CategoryTable: React.FC<CategoryTableProps> = ({ data, profile }) => {
     const columns = useMemo(
+        // Memoiza las columnas de la tabla con la propiedad isSuperAdmin del perfil.
         () => categoryColumns(profile.isSuperAdmin),
+        // El array de dependencias solo contiene el perfil, por lo que si el perfil cambia,
+        // se volverán a calcular las columnas.
         [profile]
     );
     const { setProfile } = useProfileStore();
 
     useEffect(() => {
+        // Guarda el perfil del usuario actual en el store.
         setProfile(profile);
     }, [profile]);
 
     return (
         <DataTable
+            // La lista de categorías que se va a renderizar.
             data={data}
+            // Las columnas de la tabla, que se memoizaron previamente.
             columns={columns}
+            // El toolbar con las acciones de la tabla.
             toolbarActions={<CategoryTableToolbarActions />}
+            // El placeholder del input de búsqueda.
             placeholder="Buscar categoría..."
         />
     );
