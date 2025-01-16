4725b3af2e4ba13bb7ea1826172a078be870192a
alxFv <worfysito15@gmail.com>
2025-01-16 11:31:32 -0500

    feat: cambios detectados nueva funcionalidad en page.tsx

--- src/app/(admin)/(catalog)/product/categorys/page.tsx
+++ src/app/(admin)/(catalog)/product/categorys/page.tsx
@@ -17,13 +17,11 @@ export default async function Page() {
   const profile = await getProfile();
 
   return (
-<>
+    <>
       {/* Encabezado */}
       <div className="mb-2 flex items-center justify-between space-y-2 flex-wrap gap-x-4">
         <div>
-          <h2 className="text-2xl font-bold tracking-tight">
-            Categorías
-          </h2>
+          <h2 className="text-2xl font-bold tracking-tight">Categorías</h2>
           <p className="text-muted-foreground">
             Lista de categorías registradas en el sistema.
           </p>
@@ -34,6 +32,6 @@ export default async function Page() {
       <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
         <CategoryTable data={categories} profile={profile} />
       </div>
-      </>
+    </>
   );
 }
