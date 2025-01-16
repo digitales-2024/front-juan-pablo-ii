ca91e476388ef42855864fa10dc1bb3c1393e6f4
JMNRA <jmedinan@unsa.edu.pe>
2025-01-16 11:19:14 -0500

    refactor: renombrar hook useAccount a useUser y actualizar la lógica para obtener datos de usuario

--- src/app/(admin)/(settings)/account/_hooks/useUser.tsx
+++ src/app/(admin)/(settings)/account/_hooks/useUser.tsx
@@ -1,12 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
-import { getAccount } from '../_actions/get-account.action';
-import { Account } from '../_interfaces/account.interface';
+import { getUser } from '../_actions/get-user.action';
+import { UserResponse } from '../_interfaces/account.interface';
 
-export const useAccount = (userId: string) => {
-  return useQuery<Account, Error>({
+export const useUser = (userId: string) => {
+  return useQuery<UserResponse, Error>({
     queryKey: ['account', userId],
     queryFn: async () => {
-      const response = await getAccount({ userId });
+      const response = await getUser({ userId });
       
       if (response.validationErrors) {
         throw new Error(Object.values(response.validationErrors).flat()[0]);
