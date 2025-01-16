514bf298a40408c7c9c2220d71b2a84197381262
EleoRomario <eleo.romario@gmail.com>
2025-01-16 09:59:18 -0500

    feat: actualizar la obtención de usuarios para utilizar la función getUsers

--- src/app/(admin)/users/page.tsx
+++ src/app/(admin)/users/page.tsx
@@ -1,18 +1,19 @@
-import { serverFetch } from "@/utils/serverFetch";
-import { User } from "./types";
 import { UsersTable } from "./_components/UsersTable";
 import { PageHeader } from "@/components/PageHeader";
+import { getUsers } from "./actions";
 
 export default async function PageUser() {
-	const [user] = await serverFetch<User[]>("/users");
-
+	const users = await getUsers();
 	return (
 		<>
 			<div className="mb-2 flex items-center justify-between space-y-2 flex-wrap gap-x-4">
-				<PageHeader title="Usuarios" description="Administra los usuarios de tu empresa"/>
+				<PageHeader
+					title="Usuarios"
+					description="Administra los usuarios de tu empresa"
+				/>
 			</div>
 			<div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
-				<UsersTable data={user} />
+				<UsersTable data={users || []} />
 			</div>
 		</>
 	);
