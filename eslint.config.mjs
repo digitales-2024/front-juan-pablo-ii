5b648c3adb0e58666c6ec2eb047bcd0ee94fa101
EleoRomario <eleo.romario@gmail.com>
2025-01-16 12:59:25 -0500

    feat: actualizar configuración de ESLint y dependencias para mejorar la calidad del código

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,43 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = tseslint.config(
+	{
+		ignores: [".next", "node_modules"],
+	},
+	eslint.configs.recommended,
+	tseslint.configs.recommendedTypeChecked,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	...tseslint.configs.stylisticTypeChecked,
+	// Next.js / React
+	...compat.extends("plugin:@next/next/recommended"),
+	{
+		rules: {
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+			],
+			"@typescript-eslint/consistent-type-definitions": "off",
+			"@typescript-eslint/consistent-indexed-object-style": "off",
+		},
+	}
+);
 
 export default eslintConfig;
