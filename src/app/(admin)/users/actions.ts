0de02b70d6fd6fd7d34e9c31ada08964fbcb075d
EleoRomario <eleo.romario@gmail.com>
2025-01-16 13:04:35 -0500

    refactor: eliminar funciones getUsers y createUser en actions.ts

--- src/app/(admin)/users/actions.ts
+++ src/app/(admin)/users/actions.ts
@@ -1,37 +1,3 @@
 "use server";
-import { serverFetch } from "@/utils/serverFetch";
-import { User, UserCreateDto } from "./types";
 
-export async function getUsers() {
-	try {
-		const [data, error] = await serverFetch<User[]>("/users");
-
-		if (error) {
-			throw new Error(String(error));
-		}
-
-		return data;
-	} catch (error) {
-		throw new Error(String(error));
-	}
-}
-export async function createUser(data: UserCreateDto) {
-	try {
-		const [response, error] = await serverFetch<User>("/users", {
-			method: "POST",
-			body: JSON.stringify(data),
-			credentials: "include",
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
-
-		if (error) {
-			throw new Error(String(error));
-		}
-
-		return response;
-	} catch (error) {
-		throw new Error(String(error));
-	}
-}
+export async function getUsers() {}
