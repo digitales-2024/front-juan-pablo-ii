92c791614f0e8c5766c044b1be82be39a1ed6017
EleoRomario <eleo.romario@gmail.com>
2025-01-16 16:22:06 -0500

    fix(usuarios): corrige tipo en parámetro de función createUser
    Cambia el tipo del parámetro de la función createUser de UserResponseDto a UserCreateDto para que coincida con la estructura de datos esperada al crear un nuevo usuario

--- src/app/(admin)/users/actions.ts
+++ src/app/(admin)/users/actions.ts
@@ -1,3 +1,38 @@
 "use server";
 
-export async function getUsers() {}
+import { http } from "@/utils/serverFetch";
+import { UserResponseDto, UserCreateDto } from "./types";
+
+type GetUsersResponse = UserResponseDto[] | { error: string };
+type CreateUserResponse = UserResponseDto | { error: string };
+
+export async function getUsers(): Promise<GetUsersResponse> {
+	try {
+		const [users, error] = await http.get<UserResponseDto[]>("/users");
+
+		if (error) {
+			return { error: error.message };
+		}
+
+		return users;
+	} catch (error) {
+		if (error instanceof Error) return { error: error.message };
+		return { error: "Error desconocido" };
+	}
+}
+
+
+export async function createUser(data: UserCreateDto): Promise<CreateUserResponse> {
+	try {
+		const [user, error] = await http.post<UserResponseDto>("/users", data);
+
+		if (error) {
+			return { error: error.message };
+		}
+
+		return user;
+	} catch (error) {
+		if (error instanceof Error) return { error: error.message };
+		return { error: "Error desconocido" };
+	}
+}
