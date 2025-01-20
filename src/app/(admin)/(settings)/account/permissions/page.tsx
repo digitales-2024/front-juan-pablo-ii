dab032cc0d6330cb5b9ec24f5099395bbc79056a
JMNRA <jmedinan@unsa.edu.pe>
2025-01-20 13:33:09 -0500

    feat: agregar encabezado para sección de permisos en la página de configuración de cuenta

--- src/app/(admin)/(settings)/account/permissions/page.tsx
+++ src/app/(admin)/(settings)/account/permissions/page.tsx
@@ -5,7 +5,14 @@ export default async function SettingsPage() {
   
   return (
     <div className="container mx-auto py-6">
-      <Permissions />
+      <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Permisos de la Cuenta</h3>      
+      </div>
+      
+        <Permissions />
     </div>
-  )
-}
+    </div>
+
+    )
+  }
