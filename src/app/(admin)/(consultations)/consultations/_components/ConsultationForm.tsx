b49cdc9e4f7546ce30f31058b90c04bed70e565b
EleoRomario <eleo.romario@gmail.com>
2025-01-21 11:29:11 -0500

    feat: mejorar el formulario de consulta con campos de fecha y hora, y optimizar la selección de servicio y paciente

--- src/app/(admin)/(consultations)/consultations/_components/ConsultationForm.tsx
+++ src/app/(admin)/(consultations)/consultations/_components/ConsultationForm.tsx
@@ -7,10 +7,20 @@ import {
 	CardTitle,
 } from "@/components/ui/card";
 import { UseFormReturn } from "react-hook-form";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
 import { ConsultationSchema } from "../type";
 import ComboboxSelect from "@/components/ui/combobox-select";
 import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import { format } from "date-fns";
 
 interface ConsultationFormProps {
 	form: UseFormReturn<ConsultationSchema>;
@@ -90,24 +100,79 @@ export default function ConsultationForm({
 					className="space-y-5"
 				>
 					<CardContent className="space-y-4">
-						<ComboboxSelect
-							useForm={true}
-							form={form}
-							name="serviceId"
-							label="Servicio"
-							description="Selecciona el servicio que deseas agendar"
-							placeholder="Selecciona un servicio"
-							options={ListServices}
+						<FormField
+							control={form.control}
+							name="date"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Fecha</FormLabel>
+									<FormControl>
+										<Input
+											value={format(
+												field.value,
+												"yyyy-MM-dd"
+											)}
+											readOnly
+											className="cursor-not-allowed"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
 						/>
-
-						<ComboboxSelect
-							useForm={true}
-							form={form}
+						<FormField
+							control={form.control}
+							name="time"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Hora</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											readOnly
+											className="cursor-not-allowed"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="serviceId"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Servicio</FormLabel>
+									<FormControl>
+										<ComboboxSelect
+											options={ListServices}
+											{...field}
+											description="Selecciona el servicio que deseas agendar"
+											placeholder="Selecciona un servicio"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
 							name="patientId"
-							label="Paciente"
-							description="Selecciona el paciente para la consulta"
-							placeholder="Selecciona un paciente"
-							options={ListPatients}
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Paciente</FormLabel>
+									<FormControl>
+										<ComboboxSelect
+											options={ListPatients}
+											value={field.value}
+											onChange={field.onChange}
+											description="Selecciona el paciente que deseas agendar"
+											placeholder="Selecciona un paciente"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
 						/>
 
 						<FormField
@@ -123,7 +188,8 @@ export default function ConsultationForm({
 										/>
 									</FormControl>
 									<FormDescription>
-										Agrega cualquier información adicional relevante
+										Agrega cualquier información adicional
+										relevante
 									</FormDescription>
 									<FormMessage />
 								</FormItem>
