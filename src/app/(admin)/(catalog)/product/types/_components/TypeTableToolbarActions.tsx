4bac5fe6a3752fa8997f2f46b7ae4933a28e3e0b
alxFv <worfysito15@gmail.com>
2025-01-21 17:32:24 -0500

    feat: cambios detectados nueva funcionalidad en TypeTableToolbarActions.tsx

--- src/app/(admin)/(catalog)/product/types/_components/TypeTableToolbarActions.tsx
+++ src/app/(admin)/(catalog)/product/types/_components/TypeTableToolbarActions.tsx
@@ -1,43 +1,39 @@
 "use client";
 
-import { TypeProduct } from "../types";
 import { type Table } from "@tanstack/react-table";
-
+import { TypeProduct } from "../types";
 import { CreateTypeDialog } from "./CreateTypeDialog";
 import { DeleteTypeDialog } from "./DeleteTypeDialog";
 import { ReactivateTypeDialog } from "./ReactivateTypeDialog";
-import { useProfileStore } from "@/app/hooks/use-profile";
+
 
 export interface TypeTableToolbarActionsProps {
-    table?: Table<TypeProduct>;
-    exportFile?: boolean;
+  table?: Table<TypeProduct>;
+
 }
 
 export function TypeTableToolbarActions({
-    table,
+  table,
 }: TypeTableToolbarActionsProps) {
-    const { profile } = useProfileStore();
-    return (
-        <div className="flex w-fit flex-wrap items-center gap-2">
-            {table && table.getFilteredSelectedRowModel().rows.length > 0 ? (
-                <>
-                    <DeleteTypeDialog
-                        types={table
-                            .getFilteredSelectedRowModel()
-                            .rows.map((row) => row.original)}
-                        onSuccess={() => table.toggleAllRowsSelected(false)}
-                    />
-                    {profile?.isSuperAdmin && (
-                        <ReactivateTypeDialog
-                            types={table
-                                .getFilteredSelectedRowModel()
-                                .rows.map((row) => row.original)}
-                            onSuccess={() => table.toggleAllRowsSelected(false)}
-                        />
-                    )}
-                </>
-            ) : null}
-            <CreateTypeDialog />
-        </div>
-    );
+  return (
+    <div className="flex w-fit flex-wrap items-center gap-2">
+      {table && table.getFilteredSelectedRowModel().rows.length > 0 ? (
+        <>
+          <DeleteTypeDialog
+            types={table
+              .getFilteredSelectedRowModel()
+              .rows.map((row) => row.original)}
+            onSuccess={() => table.toggleAllRowsSelected(false)}
+          />
+          <ReactivateTypeDialog
+            types={table
+              .getFilteredSelectedRowModel()
+              .rows.map((row) => row.original)}
+            onSuccess={() => table.toggleAllRowsSelected(false)}
+          />
+        </>
+      ) : null}
+      <CreateTypeDialog />
+    </div>
+  );
 }
