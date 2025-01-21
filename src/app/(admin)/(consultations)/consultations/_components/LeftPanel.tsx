64b44ed645c89824e5dd5025762505c143ec6f53
EleoRomario <eleo.romario@gmail.com>
2025-01-21 10:11:37 -0500

    feat: eliminar propiedad showForm del componente LeftPanel para simplificar la interfaz

--- src/app/(admin)/(consultations)/consultations/_components/LeftPanel.tsx
+++ src/app/(admin)/(consultations)/consultations/_components/LeftPanel.tsx
@@ -3,26 +3,23 @@ import { es } from "date-fns/locale";
 import { Calendar } from "lucide-react";
 
 interface LeftPanelProps {
-	showForm: boolean;
 	date: Date;
 	time: string;
 }
-export default function LeftPanel({ showForm, date, time }: LeftPanelProps) {
+export default function LeftPanel({ date, time }: LeftPanelProps) {
 	return (
 		<div className="grid gap-2 h-fit ">
 			<p className="text-gray-12 text-2xl font-bold">
 				Crear una consulta
 			</p>
-			{!showForm && (
-				<div className="flex text-gray-12">
-					<Calendar className="size-4 mr-2" />
-					<div className="flex flex-col text-sm font-semibold">
-						<p>
-							{format(date, "PPP", { locale: es })} a las {time}
-						</p>
-					</div>
+			<div className="flex text-gray-12">
+				<Calendar className="size-4 mr-2" />
+				<div className="flex flex-col text-sm font-semibold">
+					<p>
+						{format(date, "PPP", { locale: es })} a las {time}
+					</p>
 				</div>
-			)}
+			</div>
 		</div>
 	);
 }
