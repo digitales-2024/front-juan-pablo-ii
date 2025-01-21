ab67074c4803a271a74ab2b247443ff806965221
EleoRomario <eleo.romario@gmail.com>
2025-01-21 09:57:53 -0500

    feat: agregar propiedad de tiempo al componente LeftPanel para mostrar la hora de la consulta

--- src/app/(admin)/(consultations)/consultations/_components/LeftPanel.tsx
+++ src/app/(admin)/(consultations)/consultations/_components/LeftPanel.tsx
@@ -5,16 +5,21 @@ import { Calendar } from "lucide-react";
 interface LeftPanelProps {
 	showForm: boolean;
 	date: Date;
+	time: string;
 }
-export default function LeftPanel({ showForm, date }: LeftPanelProps) {
+export default function LeftPanel({ showForm, date, time }: LeftPanelProps) {
 	return (
-		<div className="grid gap-3">
-			<p className="text-gray-12 text-2xl font-bold">Demo</p>
-			{showForm && (
+		<div className="grid gap-2 h-fit ">
+			<p className="text-gray-12 text-2xl font-bold">
+				Crear una consulta
+			</p>
+			{!showForm && (
 				<div className="flex text-gray-12">
 					<Calendar className="size-4 mr-2" />
 					<div className="flex flex-col text-sm font-semibold">
-						<p>{format(date, "PPPp", { locale: es })}</p>
+						<p>
+							{format(date, "PPP", { locale: es })} a las {time}
+						</p>
 					</div>
 				</div>
 			)}
