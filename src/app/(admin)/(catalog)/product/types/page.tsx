07fb8901e386aec5b2470d11d5db65bf1f90a744
alxFv <worfysito15@gmail.com>
2025-01-22 15:18:13 -0500

    feat: cambios detectados nueva funcionalidad en page.tsx

--- src/app/(admin)/(catalog)/product/types/page.tsx
+++ src/app/(admin)/(catalog)/product/types/page.tsx
@@ -1,20 +1,15 @@
-import { getAllTypeProducts } from "./actions";
-import TypeTable from "./_components/TypeTable";
-import { TypeProduct } from "./types";
-import { getProfile } from "@/app/(account)/actions";
+import { TypeTable } from "./_components/TypeTable";
+import { getTypeProducts } from "./actions";
+import { notFound } from "next/navigation";
 
-/**
- * Página que muestra una lista de tipos de productos registrados en el sistema.
- *
- * Esta página utiliza la función `getAllTypeProducts` para obtener todos los tipos de productos
- * y `getProfile` para obtener el perfil del usuario actual.
- *
- * Devuelve un JSX que contiene una tabla con los tipos de productos y un shell que contiene
- * una barra de navegación y un pie de página.
- */
+
+
 export default async function Page() {
-  const typeProducts: TypeProduct[] = await getAllTypeProducts();
-  const profile = await getProfile();
+  const typeProducts= await getTypeProducts();
+
+  if (!typeProducts || 'error' in typeProducts) {
+    notFound();
+  }
 
   return (
     <>
@@ -32,7 +27,7 @@ export default async function Page() {
 
       {/* Contenido principal */}
       <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <TypeTable data={typeProducts} profile={profile} />
+        <TypeTable data={typeProducts} />
       </div>
     </>
   );
