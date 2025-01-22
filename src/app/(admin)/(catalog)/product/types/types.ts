d8fb7f82227bdf5d9f9e6ce3a18f0e79ca718147
alxFv <worfysito15@gmail.com>
2025-01-22 15:18:13 -0500

    feat: cambios detectados nueva funcionalidad en types.ts

--- src/app/(admin)/(catalog)/product/types/types.ts
+++ src/app/(admin)/(catalog)/product/types/types.ts
@@ -1,5 +1,12 @@
 import { components } from "@/types/api";
 import { z } from "zod";
+// Tipos DTO
+export type TypeProduct = components["schemas"]["TypeProduct"];
+export type CreateTypeProductDto = components["schemas"]["CreateTypeProductDto"];
+export type UpdateTypeProductDto = components["schemas"]["UpdateTypeProductDto"];
+export type DeleteTypeProductDto = components["schemas"]["DeleteTypeProductDto"];
+export type ReactivateTypeProductDto = components["schemas"]["DeleteTypeProductDto"];
+export type TypeProductResponse = components["schemas"]["TypeProductResponse"];
 
 // Esquema de validación para la creación de productos
 export const TypeProductCreateSchema = z.object({
@@ -12,10 +19,3 @@ export const TypeProductUpdateSchema = z.object({
     name: z.string().min(1, { message: "El nombre es requerido" }),
     description: z.string().optional(),
 }) satisfies z.ZodType<UpdateTypeProductDto>;
-
-// Tipos DTO
-export type TypeProduct = components["schemas"]["TypeProduct"];
-export type CreateTypeProductDto = components["schemas"]["CreateTypeProductDto"];
-export type UpdateTypeProductDto = components["schemas"]["UpdateTypeProductDto"];
-export type DeleteTypeProductDto = components["schemas"]["DeleteTypeProductDto"];
-export type ReactivateTypeProductDto = components["schemas"]["DeleteTypeProductDto"];
