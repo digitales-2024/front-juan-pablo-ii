030da2f4706e90642a76bb9db959eaabb871316e
alxFv <worfysito15@gmail.com>
2025-01-22 15:18:13 -0500

    feat: cambios detectados nueva funcionalidad en TypeTableColumns.tsx

--- src/app/(admin)/(catalog)/product/types/_components/TypeTableColumns.tsx
+++ src/app/(admin)/(catalog)/product/types/_components/TypeTableColumns.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { TypeProduct } from "../types";
+import {TypeProductResponse } from "../types";
 import { type ColumnDef } from "@tanstack/react-table";
 import { Ellipsis, RefreshCcwDot, Trash } from "lucide-react";
 import { useState } from "react";
@@ -16,16 +16,14 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { cn } from "@/lib/utils";
-
 import { DataTableColumnHeader } from "@/components/data-table/DataTableColumnHeader";
 import { Badge } from "@/components/ui/badge";
 import { DeleteTypeDialog } from "./DeleteTypeDialog";
 import { ReactivateTypeDialog } from "./ReactivateTypeDialog";
 import { UpdateTypeSheet } from "./UpdateCategorySheet";
 
-export const typeColumns = (): ColumnDef<TypeProduct>[] => {
-  const columns: ColumnDef<TypeProduct>[] = [
+export const typeColumns = (): ColumnDef<TypeProductResponse>[] => [
+  
     {
       id: "select",
       size: 10,
@@ -73,36 +71,14 @@ export const typeColumns = (): ColumnDef<TypeProduct>[] => {
     {
       id: "Descripción",
       accessorKey: "description",
-      accessorFn: (row) => row.name,
+      accessorFn: (row) => row.description,
       header: ({ column }) => (
         <DataTableColumnHeader column={column} title="Descripción" />
       ),
-
-      cell: function Cell({ row }) {
-        const description = row.getValue("Descripción") as string;
-        const [expanded, setExpanded] = useState(false);
-
-        const handleToggle = () => {
-          setExpanded(!expanded);
-        };
-        return (
-          <div
-            className={cn(
-              "w-72 truncate",
-              expanded ? "whitespace-normal" : "whitespace-nowrap"
-            )}
-            onClick={handleToggle}
-          >
-            {description ? (
-              description
-            ) : (
-              <span className="text-xs text-slate-300">Sin descripción</span>
-            )}
-          </div>
-        );
-      },
+      cell: ({ row }) => (
+        <span className="capitalize"> {row.original.description}</span>
+      ),
     },
-
     {
       id: "isActive",
       accessorKey: "isActive",
@@ -135,6 +111,7 @@ export const typeColumns = (): ColumnDef<TypeProduct>[] => {
         const [showEditDialog, setShowEditDialog] = useState(false);
 
         const { isActive } = row.original;
+        const isSuperAdmin = true;
         return (
           <div>
             <div>
@@ -205,9 +182,9 @@ export const typeColumns = (): ColumnDef<TypeProduct>[] => {
           </div>
         );
       },
+      enableSorting: false,
+      enableHiding: false,
       enablePinning: true,
     },
   ];
-
-  return columns;
-};
+ 
