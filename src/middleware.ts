99b870705d5f81d2f22ec165fc071f24669c22c0
JMNRA <jmedinan@unsa.edu.pe>
2025-01-22 14:46:38 -0500

    feat: implementar manejo de cookies en el middleware y mejorar la lógica de autenticación

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-base-to-string */
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
 import { jwtDecode } from "jwt-decode";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
@@ -94,6 +96,119 @@ const EXCLUDED_REDIRECT_ROUTES = ["/", "/sign-in"];
 // 	return NextResponse.next();
 // }
 
+// export async function middleware(request: NextRequest) {
+//   const access_token = request.cookies.get("access_token");
+//   const refresh_token = request.cookies.get("refresh_token");
+//   const isAuthenticated = !!access_token && !!refresh_token;
+//   const { pathname } = request.nextUrl;
+
+//   devlog("middleware hit");
+
+//   // Si estamos en una ruta pública (sign-in) y el usuario está autenticado
+//   // redirigimos al home o a la última URL visitada
+//   if (PUBLIC_ROUTES.includes(pathname) && isAuthenticated) {
+//     devlog("ruta publica, autenticado, continue");
+
+//     const lastVisitedUrl = request.cookies.get("lastUrl")?.value ?? "/";
+//     const nextResponse = NextResponse.redirect(
+//       new URL(lastVisitedUrl, request.url)
+//     );
+//     nextResponse.cookies.delete("lastUrl");
+//     return nextResponse;
+//   }
+
+//   // Si NO estamos en una ruta pública y el usuario NO está autenticado
+//   // guardamos la URL actual (si no está excluida) y redirigimos a sign-in
+//   if (!PUBLIC_ROUTES.includes(pathname) && !isAuthenticated) {
+//     devlog("ruta privada, no autenticado, continue");
+
+//     const response = NextResponse.redirect(new URL("/sign-in", request.url));
+
+//     // Solo guardamos la URL si no está en la lista de excluidas
+//     if (!EXCLUDED_REDIRECT_ROUTES.includes(pathname)) {
+//       response.cookies.set("lastUrl", pathname);
+//     }
+
+//     return response;
+//   }
+
+//   // Si estamos en una ruta publica, y el usuario no esta autenticado, continuar
+//   if (PUBLIC_ROUTES.includes(request.nextUrl.pathname)) {
+//     devlog("ruta publica, no autenticado, continue");
+
+//     return NextResponse.next();
+//   }
+
+//   devlog("session check");
+
+//   // En este punto se cumple que:
+//   // - Estamos en una ruta privada, y el usuario esta "autenticado"
+
+//   // Si no existe access_token o refresh_token,
+//   // redirigir a login
+//   if (!access_token || !refresh_token) {
+//     devlog(
+//       "no access/refresh token. access: " +
+//         // eslint-disable-next-line @typescript-eslint/restrict-plus-operands, @typescript-eslint/no-base-to-string
+//         access_token +
+//         ", refresh: " +
+//         // eslint-disable-next-line @typescript-eslint/restrict-plus-operands, @typescript-eslint/no-base-to-string
+//         refresh_token
+//     );
+
+//     const redirectUrl = EXCLUDED_REDIRECT_ROUTES.includes(pathname)
+//       ? "/"
+//       : pathname;
+//     return logoutAndRedirectLogin(request, redirectUrl);
+//   }
+
+//   // Si el access_token expira en 30s o menos,
+//   // intentar refrescarlo
+//   if (tokenExpiration(access_token.value) < 30) {
+//     devlog("access_token exp: less than 30");
+
+//     // Si refresh_token expira en 5s o menos,
+//     // eliminar todas las cookies y redirigir a login
+//     if (tokenExpiration(refresh_token.value) < 5) {
+//       devlog("refresh_token exp: less than 5");
+
+//       const redirectUrl = EXCLUDED_REDIRECT_ROUTES.includes(pathname)
+//         ? "/"
+//         : pathname;
+//       return logoutAndRedirectLogin(request, redirectUrl);
+//     }
+
+//     const [newCookies, err] = await refresh(
+//       access_token.value,
+//       refresh_token.value
+//     );
+//     if (err) {
+//       devlog("refresh failure");
+
+//       console.log(err);
+//       const redirectUrl = EXCLUDED_REDIRECT_ROUTES.includes(pathname)
+//         ? "/"
+//         : pathname;
+//       return logoutAndRedirectLogin(request, redirectUrl);
+//     }
+
+//     devlog("resetting cookies & forward");
+
+//     const response = NextResponse.next();
+//     newCookies.forEach((cookie) => {
+//       devlog("Set-Cookie: " + cookie);
+//       response.headers.append("Set-Cookie", cookie);
+//     });
+
+//     return response;
+//   }
+
+//   devlog("session valid, forward");
+
+//   // access_token es valido, continuar
+//   return NextResponse.next();
+// }
+
 export async function middleware(request: NextRequest) {
   const access_token = request.cookies.get("access_token");
   const refresh_token = request.cookies.get("refresh_token");
@@ -147,10 +262,8 @@ export async function middleware(request: NextRequest) {
   if (!access_token || !refresh_token) {
     devlog(
       "no access/refresh token. access: " +
-        // eslint-disable-next-line @typescript-eslint/restrict-plus-operands, @typescript-eslint/no-base-to-string
         access_token +
         ", refresh: " +
-        // eslint-disable-next-line @typescript-eslint/restrict-plus-operands, @typescript-eslint/no-base-to-string
         refresh_token
     );
 
@@ -195,7 +308,12 @@ export async function middleware(request: NextRequest) {
     const response = NextResponse.next();
     newCookies.forEach((cookie) => {
       devlog("Set-Cookie: " + cookie);
-      response.headers.append("Set-Cookie", cookie);
+      const { name, value, options } = parseSetCookie(cookie);
+      response.cookies.set({
+        name,
+        value,
+        ...options,
+      });
     });
 
     return response;
@@ -205,6 +323,38 @@ export async function middleware(request: NextRequest) {
 
   // access_token es valido, continuar
   return NextResponse.next();
+}
+
+function parseSetCookie(cookieString: string) {
+  const pairs: [string, string | boolean][] = cookieString
+    .split(";")
+    .map((pair) => pair.trim())
+    .map((pair) => {
+      const [key, ...values] = pair.split("=");
+      return [key.toLowerCase(), values.join("=") || true];
+    });
+
+  // Get the first pair which has the cookie name and value
+  const [cookieName, cookieValue] = pairs[0];
+  const cookieMap = new Map(pairs.slice(1));
+
+  return {
+    name: cookieName,
+    value: cookieValue as string,
+    options: {
+      path: cookieMap.get("path") as string,
+      maxAge: cookieMap.has("max-age")
+        ? parseInt(cookieMap.get("max-age") as string)
+        : undefined,
+      expires: cookieMap.has("expires")
+        ? new Date(cookieMap.get("expires") as string)
+        : undefined,
+      httpOnly: cookieMap.get("httponly") === true,
+      sameSite: cookieMap.has("samesite")
+        ? ((cookieMap.get("samesite") as string).toLowerCase() as "strict")
+        : undefined,
+    },
+  };
 }
 
 function devlog(message: string) {
