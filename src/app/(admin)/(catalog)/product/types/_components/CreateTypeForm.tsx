df66c39175b47914822ee9de2ca240de0053fb07
alxFv <worfysito15@gmail.com>
2025-01-22 15:18:13 -0500

    feat: cambios detectados nueva funcionalidad en CreateTypeForm.tsx

--- src/app/(admin)/(catalog)/product/types/_components/CreateTypeForm.tsx
+++ src/app/(admin)/(catalog)/product/types/_components/CreateTypeForm.tsx
@@ -1,82 +1,93 @@
 "use client";
 
 import { UseFormReturn } from "react-hook-form";
-import { CreateTypeProductInput } from "../types";
+import { CreateTypeProductDto } from "../types";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
+import { Box, List } from "lucide-react";
 
 interface CreateTypeFormProps
-    extends Omit<React.ComponentPropsWithRef<"form">, "onSubmit"> {
-    children: React.ReactNode;
-    form: UseFormReturn<CreateTypeProductInput>;
-    onSubmit: (data: CreateTypeProductInput) => void;
+  extends Omit<React.ComponentPropsWithRef<"form">, "onSubmit"> {
+  children: React.ReactNode;
+  form: UseFormReturn<CreateTypeProductDto>;
+  onSubmit: (data: CreateTypeProductDto) => void;
 }
 
 export const CreateTypeForm = ({
-    children,
-    form,
-    onSubmit,
+  children,
+  form,
+  onSubmit,
 }: CreateTypeFormProps) => {
-    return (
-        <Form {...form}>
-            <form
-                onSubmit={form.handleSubmit(onSubmit)}
-                className="space-y-8 p-1"
-            >
-                <div className="flex flex-col gap-6 p-4 sm:p-0">
-                    {/* Campo de Nombre */}
-                    <FormField
-                        control={form.control}
-                        name="name"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel htmlFor="name">
-                                    Nombre del Tipo de Producto
-                                </FormLabel>
-                                <FormControl>
-                                    <Input
-                                        id="name"
-                                        placeholder="Ingrese el nombre del tipo de producto"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 p-1">
+        <div className="flex flex-col gap-6 p-4 sm:p-0">
+          {/* Campo de Nombre */}
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="name">
+                  Nombre del Tipo de Producto
+                </FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    {/* icon */}
+                    <Box className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 transform text-muted-foreground" />
+                    {/* input */}
+                    <Input
+                      id="name"
+                      placeholder="Ingrese el nombre del tipo de producto"
+                      {...field}
+                      className="pl-8"
                     />
-                    {/* Campo de Descripción */}
-                    <FormField
-                        control={form.control}
-                        name="description"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel htmlFor="description">
-                                    Descripción del Tipo de Producto
-                                </FormLabel>
-                                <FormControl>
-                                    <Textarea
-                                        id="description"
-                                        placeholder="Ingrese la descripción del tipo de producto"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Campo de Descripción */}
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="description">
+                  Descripción del Tipo de Producto
+                </FormLabel>
+                <FormControl>
+                <div className="relative">
+                    {/* icon */}
+                    <List className="absolute left-2 top-1/2 h-4 w-4 -translate-y-1/2 transform text-muted-foreground" />
+                    {/* input */}
+                    <Textarea
+                      id="description"
+                      placeholder="Ingrese la descripción del tipo de producto"
+                      {...field}
+                      className="pl-8"
                     />
-                    {/* Agrega más campos según sea necesario */}
-                </div>
-                {children}
-            </form>
-        </Form>
-    );
+                  </div>
+
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {/* Agrega más campos según sea necesario */}
+        </div>
+        {children}
+      </form>
+    </Form>
+  );
 };
