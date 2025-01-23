0ddb8bc9561c00d46f7cd586c52000f2cce12079
alxFv <worfysito15@gmail.com>
2025-01-23 13:15:48 -0500

    feat: cambios detectados nueva funcionalidad en TypeTable.tsx

--- src/app/(admin)/(catalog)/product/product-types/_components/TypeTable.tsx
+++ src/app/(admin)/(catalog)/product/product-types/_components/TypeTable.tsx
@@ -3,20 +3,21 @@ import { DataTable } from "@/components/data-table/DataTable";
 import { useMemo } from "react";
 import { typeColumns } from "./TypeTableColumns";
 import { TypeTableToolbarActions } from "./TypeTableToolbarActions";
-import {  TypeProductResponse } from "../types";
+import { TypeProductResponse } from "../types";
 
 interface TData {
-    data: TypeProductResponse[];
+  data: TypeProductResponse[];
 }
 
 export function TypeTable({ data }: TData) {
-const columns = useMemo(() => typeColumns(), []);
-  
-    return (
-        <DataTable
-            data={data|| []}
-            columns={columns}
-            toolbarActions={<TypeTableToolbarActions />}
-        />
-    );
-};
+  const columns = useMemo(() => typeColumns(), []);
+
+  return (
+    <DataTable
+      data={data ?? []}
+      columns={columns}
+      toolbarActions={(table) => <TypeTableToolbarActions table={table} />}
+      placeholder="Buscar tipos de productos..."
+    />
+  );
+}
