24c3e238dc6b0e916dfce74bd0efdbab01765b90
JMNRA <jmedinan@unsa.edu.pe>
2025-01-23 14:38:41 -0500

    deploy: solucionando bugs xd

--- src/app/(admin)/(catalog)/product/product-types/page.tsx
+++ src/app/(admin)/(catalog)/product/product-types/page.tsx
@@ -25,7 +25,7 @@ export default function TypeProductsPage() {
 
       {/* Contenido principal */}
       <div className="-mx-4 flex-1 overflow-auto px-4 py-1 lg:flex-row lg:space-x-12 lg:space-y-0">
-        <TypeTable data={response} />
+        <TypeTable data={response ?? []} />
       </div>
     </>
   );
