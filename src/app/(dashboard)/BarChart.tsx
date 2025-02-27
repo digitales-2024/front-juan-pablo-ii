a75a910024ec2e5e1a027607ea5701712616594e
alxFv <worfysito15@gmail.com>
2025-02-27 12:47:43 -0500

    feat: cambios detectados nueva funcionalidad en BarChart.tsx

--- src/app/(dashboard)/BarChart.tsx
+++ src/app/(dashboard)/BarChart.tsx
@@ -44,7 +44,7 @@ export function KPIBarChart() {
               tickLine={false}
               tickMargin={10}
               axisLine={false}
-              tickFormatter={(value) => value.slice(0, 3)}
+              tickFormatter={(value: string) => value.slice(0, 3)}
             />
             <ChartTooltip
               cursor={false}
