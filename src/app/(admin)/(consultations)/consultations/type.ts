0c0ac182933cb3b919335c97899ba31e7ba3e239
JMNRA <jmedinan@unsa.edu.pe>
2025-02-28 17:09:28 -0500

    fix: type Consultationschema actualizado

--- src/app/(admin)/(consultations)/consultations/type.ts
+++ src/app/(admin)/(consultations)/consultations/type.ts
@@ -7,6 +7,8 @@ export const consultationsSchema = z.object({
 	branchId: z.string().min(1, { message: "Debe seleccionar una sucursal" }),
 	description: z.string().min(1, { message: "La descripción es requerida" }),
 	staffId: z.string().min(1, { message: "Debe seleccionar un médico" }),
+	patientId: z.string().min(1, { message: "Debe seleccionar un paciente" }),
+
 });
 
 export type ConsultationSchema = z.infer<typeof consultationsSchema>;
