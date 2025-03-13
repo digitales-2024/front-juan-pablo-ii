1c636819532c8bb18b5da140e60a20cfb1059e6a
alxFv <worfysito15@gmail.com>
2025-03-13 09:53:00 -0500

    feat: cambios detectados nueva funcionalidad en KPI.actions.ts

--- src/app/(dashboard)/_actions/KPI.actions.ts
+++ src/app/(dashboard)/_actions/KPI.actions.ts
@@ -41,13 +41,12 @@ export async function getCitasPorSucursalKPI(): Promise<
   ApiResponse<PacientesPorSucursalData>
 > {
   try {
-
     // Realizar la petición al backend
     const url = "/dashboard/citas-por-sucursal";
 
     const [response, error] = await http.get(url);
 
-  /*   console.log("🚀 ~ response kip citas :", response) */
+    /*   console.log("🚀 ~ response kip citas :", response) */
 
     // Si hay error, retornarlo
     if (error) {
@@ -56,6 +55,42 @@ export async function getCitasPorSucursalKPI(): Promise<
 
     // Si todo está bien, retornar los datos
     return { data: (response as { data: PacientesPorSucursalData }).data };
+  } catch (error) {
+    console.error("Error fetching patient data:", error);
+    return { error: "Error inesperado al obtener datos" };
+  }
+}
+
+// Añadir esta interfaz a tu archivo de interfaces
+// filepath: /home/userdev/Documentos/userDev/proyectoJPii/front-juan-pablo-ii/src/app/(dashboard)/_interfaces/KPI.interface.ts
+export interface TopServiciosData {
+  serviceName: string;
+  JLBYR: number;
+  Yanahuara: number;
+}
+
+// Añadir esta función al archivo de acciones
+/**
+ * Obtiene los datos de los 12 servicios más demandados por sucursal
+ * @returns Datos formateados o un objeto de error
+ */
+export async function getTopServicesBySucursalKPI(): Promise<
+  ApiResponse<TopServiciosData[]>
+> {
+  try {
+    // Realizar la petición al backend
+    const url = "/dashboard/top-servicios-por-sucursal";
+
+    const [response, error] = await http.get(url);
+    console.log("🚀 ~ response:", response)
+
+    // Si hay error, retornarlo
+    if (error) {
+      return { error: "Error al obtener datos de top servicios por sucursal" };
+    }
+
+    // Si todo está bien, retornar los datos
+    return { data: (response as { data: TopServiciosData[] }).data };
   } catch (error) {
     console.error("Error fetching patient data:", error);
     return { error: "Error inesperado al obtener datos" };
