e15661855f6d50a2be36c5a269dba057e46360ab
Gonzalo Gutiérrez Castillo <gonzalogut.99@gmail.com>
2025-03-14 07:51:20 -0500

    feat: agregar mutaciones para la creación de órdenes de prescripción y citas médicas en el sistema de facturación

--- src/app/(admin)/(payment)/orders/_hooks/useBilling.ts
+++ src/app/(admin)/(payment)/orders/_hooks/useBilling.ts
@@ -1,3 +1,4 @@
+import { CreateMedicalAppointmentBillingDto, CreatePrescriptionBillingLocalDto } from "./../_interfaces/order.interface";
 // interface UpdateOrderVariables {
 //     id: string;
 //     data: UpdateOrderDto;
@@ -5,15 +6,24 @@
 
 import { BaseApiResponse } from "@/types/api/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { CreateProductSaleBillingDto, CreateMedicalAppointmentBillingDto, Order } from "../_interfaces/order.interface";
-import { createProductSaleOrder, createMedicalAppointmentOrder } from "../_actions/billing.actions";
+import {
+  CreateProductSaleBillingDto,
+  Order,
+} from "../_interfaces/order.interface";
+import {
+  createMedicalAppointmentOrder,
+  createPrescriptionOrder,
+  createProductSaleOrder,
+} from "../_actions/billing.actions";
 import { toast } from "sonner";
-
 
 export const useBilling = () => {
   const queryClient = useQueryClient();
-
-  const createSaleOrderMutation = useMutation<BaseApiResponse<Order>, Error, CreateProductSaleBillingDto>({
+  const createSaleOrderMutation = useMutation<
+    BaseApiResponse<Order>,
+    Error,
+    CreateProductSaleBillingDto
+  >({
     mutationFn: async (data) => {
       const response = await createProductSaleOrder(data);
       if ("error" in response) {
@@ -36,28 +46,58 @@ export const useBilling = () => {
     },
     onError: (error) => {
       toast.error(error.message);
-    }
+    },
   });
 
-  const createMedicalAppointmentOrderMutation = useMutation<BaseApiResponse<Order>, Error, CreateMedicalAppointmentBillingDto>({
+  const createPrescriptionOrderMutation = useMutation<
+    BaseApiResponse<Order>,
+    Error,
+    CreatePrescriptionBillingLocalDto
+  >({
     mutationFn: async (data) => {
-      const response = await createMedicalAppointmentOrder(data);
+      const response = await createPrescriptionOrder(data);
       if ("error" in response) {
         throw new Error("Error del servidor: " + response.error);
       }
       return response;
     },
     onSuccess: async (res) => {
+      // const detailedOrder = await getDetailedOrderById(res.data.id);
+      // if ("error" in detailedOrder) {
+      //   throw new Error(detailedOrder.error);
+      // }
+      // queryClient.setQueryData<DetailedOrder[] | undefined>(
+      //   ["detailed-orders"], (oldOrders) => {
+      //     if (!oldOrders) return detailedOrder;
+      //     return [...oldOrders, ...detailedOrder];
+      // });
       await queryClient.invalidateQueries({ queryKey: ["orders"] });
       toast.success(res.message);
     },
     onError: (error) => {
       toast.error(error.message);
-    }
+    },
   });
 
+    const createMedicalAppointmentOrderMutation = useMutation<BaseApiResponse<Order>, Error, CreateMedicalAppointmentBillingDto>({
+      mutationFn: async (data) => {
+        const response = await createMedicalAppointmentOrder(data);
+        if ("error" in response) {
+          throw new Error("Error del servidor: " + response.error);
+        }
+        return response;
+      },
+      onSuccess: async (res) => {
+        await queryClient.invalidateQueries({ queryKey: ["orders"] });
+        toast.success(res.message);
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      }
+    });
   return {
     createSaleOrderMutation,
+    createPrescriptionOrderMutation,
     createMedicalAppointmentOrderMutation
   };
 };
